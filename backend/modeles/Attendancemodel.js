2c0071e50e327abaeb761e31a9b0c54f91047330
Nimesha511 <nimeshatharushi222@gmail.com>
2025-04-20 12:51:17 +0530

    Update Attendancemodel.js
    updated

--- backend/modeles/Attendancemodel.js
+++ backend/modeles/Attendancemodel.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const attendanceSchema = new mongoose.Schema({
- 
     name: {
         type: String,
         required: true,
@@ -12,14 +11,19 @@ const attendanceSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        enum: ["Present","Absent","Sick"],
+        enum: ["Present", "Absent", "Sick"],
         required: true,
     },
     reason: {
         type: String,
         required: true,
-        
     },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
 });
 
-module.exports = mongoose.model("Attendancemodel", attendanceSchema);
+const Attendance = mongoose.model("Attendance", attendanceSchema);
+
+export default Attendance;
